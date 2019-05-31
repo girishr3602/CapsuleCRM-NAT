0465b5ae673f1b74f0a05d71d598cd99b8dc6581
girishr3602 <girish.r3602@gmail.com>
2019-05-31 11:37:00 +0530

    Girish: Adding log4j.properties

--- src/test/java/com/capsulecrm/qa/testcases/TestRun.java
+++ src/test/java/com/capsulecrm/qa/testcases/TestRun.java
@@ -3,10 +3,13 @@ package com.capsulecrm.qa.testcases;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 import com.capsulecrm.qa.base.TestBase;
 
 public class TestRun extends TestBase {
+	
+	Logger log = Logger.getLogger(TestRun.class);
 
 	public TestRun() {
 		super();
@@ -15,17 +18,21 @@ public class TestRun extends TestBase {
 	@BeforeMethod
 	public void setUp() {
 		initialization();
+		log.info("Driver initialized");
 	}
 	
 	@Test
 	public void testRun() {
 		String OS_NAME = System.getProperty("os.name");
+		log.info("Get the OS");
 		
 		System.out.println(OS_NAME);
 		if(OS_NAME.contains("Windows")) {
 			System.out.println("Opearating System is : " + OS_NAME);
+			log.info("OS is Windows");
 		}else if(OS_NAME.contains("Linux")) {
 			System.out.println("Opearating System is : " + OS_NAME);
+			log.info("OS is Linux");
 		}
 
 	}
@@ -33,5 +40,6 @@ public class TestRun extends TestBase {
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
+		log.info("Close driver");
 	}
 }
